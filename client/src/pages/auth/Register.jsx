a5322b3120ab8d6ac33c37a829a7447f633d17f2
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2020-11-13 08:54:21 +0900

    sending Email for login and firebase setting

--- client/src/pages/auth/Register.jsx
+++ client/src/pages/auth/Register.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { auth } from '../../firebase'
+import { toast, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 const Register = () => {
+  const [email, setEmail] = useState('')
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const config = {
+      url: 'http://localhost:3000/register/complate',
+      handleCodeInApp: true
+    }
+    await auth.sendSignInLinkToEmail(email, config)
+    toast.success(`${ email }宛に登録用のメールを送信しました。`)
+    //save user Email LocalStorage
+    window.localStorage.setItem('emailForRegistation', email)
+    //clear state
+    setEmail("")
+  }
+
+  const registerform = () => <form onSubmit={handleSubmit}>
+    <input
+      type="email"
+      className="form-control"
+      value={email}
+      onChange={e => setEmail(e.target.value)}
+      autoFocus
+    />
+    <button type="submit" className="btn btn-raised">Sign Up</button>
+  </form>
   return (
-    <div>
-      <p>Register</p>
+    <div className="container p-5">
+      <div className="row">
+        <div className="col-md-6 offset-md-3">
+          <h4>Sign Up</h4>
+          <ToastContainer/>
+          {registerform()}
+        </div>
+      </div>
     </div>
   )
 }
