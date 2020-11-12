c9a127f3de95aa8d9b9219aa045bdabe909fee67
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2020-11-12 14:38:59 +0900

    nav design and link

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,18 @@ import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import { Register, Login } from './pages/auth/index'
 import Home from './pages/Home'
+import Header from './components/nav/Header'
 
 const App = () => {
   return (
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/login" component={Login} />
-      <Route exact path="/register" component={Register}/>
-    </Switch>
+    <>
+      <Header/>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/login" component={Login} />
+        <Route exact path="/register" component={Register}/>
+      </Switch>
+    </>
   )
 }
 
