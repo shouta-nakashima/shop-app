4595fea436302ea9a3aa667cc1bcd73282f94613
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2020-11-13 13:02:30 +0900

    complate register pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,21 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
-import { Register, Login } from './pages/auth/index'
+import { Register, Login, RegisterComplate } from './pages/auth/index'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import Home from './pages/Home'
 import Header from './components/nav/Header'
 
 const App = () => {
   return (
     <>
-      <Header/>
+      <Header />
+      <ToastContainer position="top-center"/>
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/login" component={Login} />
-        <Route exact path="/register" component={Register}/>
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/register/complate" component={RegisterComplate}/>
       </Switch>
     </>
   )
