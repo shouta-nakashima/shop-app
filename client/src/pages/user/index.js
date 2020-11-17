6dbfd8a7c6912e8573da9c7b9e7e71303ee601d9
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2020-11-17 16:50:02 +0900

    update user password

--- client/src/pages/user/index.js
+++ client/src/pages/user/index.js
@@ -1,1 +1,3 @@
-export {default as History} from './History'
+export { default as History } from './History'
+export { default as Password } from './Password'
+export {default as Wishlist} from './Wishlist'
