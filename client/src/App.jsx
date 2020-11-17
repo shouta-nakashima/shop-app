8ffacfe895e613bdcb800443cdb04ffbb83b20bd
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2020-11-17 13:45:55 +0900

    current user endpoint

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,8 @@ import 'react-toastify/dist/ReactToastify.css'
 import Home from './pages/Home'
 import Header from './components/nav/Header'
 import { auth } from './firebase'
-import {useDispatch} from 'react-redux'
+import { useDispatch } from 'react-redux'
+import {currentUser} from './functions/auth'
 
 const App = () => {
 
@@ -17,13 +18,20 @@ const App = () => {
       if (user) {
         const idTokenResult = await user.getIdTokenResult()
         console.log("user", user);
-        dispatch({
-          type: "LOGGED_IN_USER",
-          payload: {
-            email: user.email,
-            token: idTokenResult.token
-          }
-        })
+        currentUser(idTokenResult.token)
+          .then((res) => {
+            dispatch({
+              type: "LOGGED_IN_USER",
+              payload: {
+                name: res.data.name,
+                email: res.data.email,
+                token: idTokenResult.token,
+                role: res.data.role,
+                _id: res.data._id,
+              },
+            });
+          })
+          .catch(err => console.log(err));
       }
     })
     return () => unsubscribe()
