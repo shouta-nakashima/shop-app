6552914babcb0d383ebf04fb7374f4adc0bc60b4
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2020-11-23 00:29:54 +0900

    product slug from router

--- client/src/pages/admin/product/UpdateProduct.jsx
+++ client/src/pages/admin/product/UpdateProduct.jsx
@@ -6,11 +6,13 @@ import { createProduct } from '../../../functions/product'
 import { ProductCreateForm } from '../../../components/forms/index'
 import { getCategories, getCategorySubs } from '../../../functions/category'
 import { FileUpload } from '../../../components/forms/index'
-import { Spin} from 'antd';
+import { Spin } from 'antd';
 
-const UpdateProduct = () => {
+
+const UpdateProduct = ({match}) => {
   const [loading, setLoading] = useState(false)
   const { user } = useSelector((state) => ({ ...state }))
+  let {slug} = match.params
   
   return (
     <Spin spinning={loading} tip="Loading..." size="large">
@@ -22,6 +24,7 @@ const UpdateProduct = () => {
           <div className="col-md-10">
             <h4 className="text-center pt-3 pb-3">Product Update</h4>
             <hr />
+            {JSON.stringify(slug)}
           </div>
         </div>
       </div>
