26893851a5703ff5b08f142a9d1ab3a10f262d58
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2020-11-23 00:11:57 +0900

    product update page

---
+++ client/src/pages/admin/product/UpdateProduct.jsx
@@ -0,0 +1,32 @@
+import React, {useState, useEffect} from 'react'
+import AdminNav from '../../../components/nav/AdminNav'
+import { toast } from 'react-toastify'
+import { useSelector } from 'react-redux'
+import { createProduct } from '../../../functions/product'
+import { ProductCreateForm } from '../../../components/forms/index'
+import { getCategories, getCategorySubs } from '../../../functions/category'
+import { FileUpload } from '../../../components/forms/index'
+import { Spin} from 'antd';
+
+const UpdateProduct = () => {
+  const [loading, setLoading] = useState(false)
+  const { user } = useSelector((state) => ({ ...state }))
+  
+  return (
+    <Spin spinning={loading} tip="Loading..." size="large">
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-2">
+            <AdminNav/>
+          </div>
+          <div className="col-md-10">
+            <h4 className="text-center pt-3 pb-3">Product Update</h4>
+            <hr />
+          </div>
+        </div>
+      </div>
+    </Spin>
+  )
+}
+
+export default UpdateProduct
