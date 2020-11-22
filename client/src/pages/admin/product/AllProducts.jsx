995776b5163a6f7a237e0ece9d47c16d75fcd6a3
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2020-11-22 23:42:24 +0900

    delete product

--- client/src/pages/admin/product/AllProducts.jsx
+++ client/src/pages/admin/product/AllProducts.jsx
@@ -3,10 +3,14 @@ import {AdminNav} from '../../../components/nav/index'
 import { getProductsByCount } from '../../../functions/product'
 import { Spin } from 'antd';
 import AdminProductCard from '../../../components/cards/AdminProductCard'
+import {removeProduct} from '../../../functions/product'
+import { toast } from 'react-toastify';
+import {useSelector} from 'react-redux'
 
 const AllProducts = () => {
   const [products, setProducts] = useState([])
   const [loading, setLoading] = useState(false)
+  const {user} = useSelector((state) => ({...state}))
 
   useEffect(() => {
     loadAllProducts()
@@ -24,6 +28,25 @@ const AllProducts = () => {
         setLoading(false)
       })
   }
+
+  const handleRemove = (slug) => {
+    //let answer = window.confirm('削除しますか？')
+    if (window.confirm('削除しますか？')) {
+      //console.log('delete product', slug);
+      removeProduct(slug, user.token)
+        .then((res) => {
+          loadAllProducts()
+          toast.error(`${res.data.title}を削除しました。`)
+        })
+        .catch((err) => {
+          console.log(err);
+          if (err.response.status === 400) {
+            setLoading(false)
+            toast.error(err.response.data);
+          }
+        })
+    }
+  }
   return (
     <Spin spinning={loading} tip="Loading..." size="large">
       <div className ="container-fluid">
@@ -36,7 +59,7 @@ const AllProducts = () => {
             <div className="row">
               {products.map(product =>
                 <div className="col-md-4 pb-3" key={product._id}>
-                  <AdminProductCard product={product} />
+                  <AdminProductCard product={product} handleRemove={ handleRemove}/>
                 </div>
               )}
             </div>
