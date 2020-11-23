bff7ee37071165b376343c3ab74855834308fb95
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2020-11-24 08:47:18 +0900

    product update endpoint

--- server/routes/product.js
+++ server/routes/product.js
@@ -6,12 +6,14 @@ const router = express.Router()
 const {authCheck, adminCheck} = require('../middlewares/auth')
 
 //controller 
-const { create, listAll, remove, read} = require('../controllers/product')
+const { create, listAll, remove, read, update} = require('../controllers/product')
+const { update } = require('../models/user')
 
 //route
 router.post('/product', authCheck, adminCheck, create)
 router.get('/products/:count', listAll)
 router.delete('/product/:slug', authCheck, adminCheck, remove)
 router.get('/product/:slug', read)
+router.put('/product/slug', authCheck, adminCheck, update)
 
 module.exports = router
