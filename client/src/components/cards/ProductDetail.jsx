46680f9a1d90c5a8544aa915c650b0601e094c05
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2020-11-25 10:57:05 +0900

    product list and listItem component

--- client/src/components/cards/ProductDetail.jsx
+++ client/src/components/cards/ProductDetail.jsx
@@ -5,11 +5,10 @@ import { HeartOutlined, ShoppingCartOutlined } from '@ant-design/icons'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 import NoImage from '../../image/no_image.png'
-
-const { Meta } = Card;
+import {ProductListItem} from './index'
 
 const ProductDetail = ({ product }) => {
-  const {title, description, images, slug} = product
+  const {title, images} = product
   return (
     <>
       <div className="col-md-7">
@@ -21,6 +20,7 @@ const ProductDetail = ({ product }) => {
         }
       </div>
       <div className="col-md-5">
+        <h1 className="bg-info p-3">{ title}</h1>
         <Card
           actions={[
             <>
@@ -31,7 +31,7 @@ const ProductDetail = ({ product }) => {
             </Link>
           ]}
         >
-          <Meta title={title} description={ description}/>
+          <ProductListItem product={ product}/>
         </Card>
       </div>
     </>
