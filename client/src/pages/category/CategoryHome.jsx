bee5196a65132432c2b8f1f79a2a9d912cccc520
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2020-11-26 16:28:26 +0900

    category home component and Get product based on category

---
+++ client/src/pages/category/CategoryHome.jsx
@@ -0,0 +1,27 @@
+import React,{useState, useEffect} from 'react'
+import {getCategory} from '../../functions/category'
+import { Link } from 'react-router-dom';
+import {ProductCard} from '../../components/cards/index';
+const CategoryHome = ({match}) => {
+  const [category, setCategory] = useState({})
+  const [products, setProducts] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  const {slug} = match.params
+
+  useEffect(() => {
+    setLoading(true)
+    getCategory(slug)
+      .then(c => {
+      console.log(JSON.stringify(c.data, null,4));
+      setCategory(c.data)
+    })
+  },[])
+  return (
+    <div>
+      <p>{slug}</p>
+    </div>
+  )
+}
+
+export default CategoryHome
