40a4eda8d019bedf3bf07f509855efd0cce3de86
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2020-11-27 16:26:40 +0900

    price filter frontend

--- client/src/pages/Shop.jsx
+++ client/src/pages/Shop.jsx
@@ -2,12 +2,19 @@ import React,{useState, useEffect} from 'react'
 import { getProductsByCount,fetchProductsByFilter } from '../functions/product'
 import {useSelector, useDispatch} from 'react-redux'
 import {ProductCard} from '../components/cards/index'
-import { Spin } from 'antd';
+import { Spin, Menu, Slider } from 'antd';
+import {DollarCircleOutlined} from '@ant-design/icons'
+
+
+const {SubMenu,ItemGroup} = Menu
 
 const Shop = () => {
   const [products, setProducts] = useState([])
   const [loading, setLoading] = useState(false)
+  const [price, setPrice] = useState([0, 0])
+  const [ok, setOk] = useState(false)
 
+  let dispatch = useDispatch()
   let {search} = useSelector((state) => ({...state}))
   const {text} = search
 
@@ -39,14 +46,53 @@ const Shop = () => {
       })
   }
 
+  //3 priceに基づいて製品をload
+  useEffect(() => {
+    //console.log('price response');
+    fetchProducts({price})
+  },[ok])
+
+  const handleSlider = (value) => {
+    dispatch({
+      type: "SEARCH_QUERY",
+      payload: {text: ""}
+    })
+    setPrice(value)
+    setTimeout(() => {
+      setOk(!ok)
+    },300)
+  }
+
   return (
     <Spin spinning={loading} tip="Loading..." size="large">
       <div className="container-fluid">
         <div className="row">
-          <div className="col-md-3">
-            searth/filter menu
+          <div className="col-md-3 pt-2">
+            <h4>Search&Filter</h4>
+            <hr/>
+            <Menu mode="inline" defaultOpenKeys={["1","2"]}>
+              <SubMenu
+                key="1"
+                title={
+                  <span className="h6">
+                    <DollarCircleOutlined /> Price
+                  </span>
+                }
+              >
+                <div>
+                  <Slider
+                    className="ml-4 mr-4"
+                    tipFormatter={(v) => `${v}円`}
+                    range
+                    value={price}
+                    onChange={handleSlider}
+                    max="400000"
+                  />
+                </div>
+              </SubMenu>
+            </Menu>
           </div>
-          <div className="col-md-9">
+          <div className="col-md-9 pt-2">
             <h4>Products</h4>
             {products.length < 1 && <p>No Products</p>}
             <div className="row pb-5">
