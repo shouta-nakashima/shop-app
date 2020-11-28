0be11d0d042925a3ee23a872215e821491dcb7f4
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2020-11-28 11:51:23 +0900

    search filter category frontend

--- client/src/pages/Shop.jsx
+++ client/src/pages/Shop.jsx
@@ -1,9 +1,10 @@
 import React,{useState, useEffect} from 'react'
-import { getProductsByCount,fetchProductsByFilter } from '../functions/product'
+import { getProductsByCount, fetchProductsByFilter } from '../functions/product'
+import {getCategories} from '../functions/category'
 import {useSelector, useDispatch} from 'react-redux'
 import {ProductCard} from '../components/cards/index'
-import { Spin, Menu, Slider } from 'antd';
-import {DollarCircleOutlined} from '@ant-design/icons'
+import { Spin, Menu, Slider, Checkbox } from 'antd';
+import {DollarCircleOutlined, DownSquareOutlined} from '@ant-design/icons'
 
 
 const {SubMenu,ItemGroup} = Menu
@@ -13,6 +14,8 @@ const Shop = () => {
   const [loading, setLoading] = useState(false)
   const [price, setPrice] = useState([0, 0])
   const [ok, setOk] = useState(false)
+  const [categories, setCategories] = useState([])
+  const [categoryIds, setCategoryIds] = useState([])
 
   let dispatch = useDispatch()
   let {search} = useSelector((state) => ({...state}))
@@ -20,6 +23,8 @@ const Shop = () => {
 
   useEffect(() => {
     loadAllProducts()
+    //fetch categories
+    getCategories().then((res) => setCategories(res.data))
   },[])
 
   //1 デフォルトの検索入力で商品を読み込む
@@ -57,10 +62,50 @@ const Shop = () => {
       type: "SEARCH_QUERY",
       payload: {text: ""}
     })
+    setCategoryIds([])
     setPrice(value)
     setTimeout(() => {
       setOk(!ok)
     },300)
+  }
+
+  //4 category に基づいて製品をload
+  const showCategories = () => categories.map((c) =>
+    <div key={c._id}>
+      <Checkbox
+        onChange={handleCheck} 
+        className="pb-2 pl-4 pr-4"
+        value={c._id}
+        name="category"
+        checked={categoryIds.includes(c._id)}
+      >
+        {c.name}
+      </Checkbox>
+      <br/>
+    </div>
+  )
+
+  const handleCheck = e => {
+    dispatch({
+      type: "SEARCH_QUERY",
+      payload: {text: ""}
+    })
+    setPrice([0,0])
+    //console.log(e.target.value);
+    let inTheState = [...categoryIds]
+    let justChecked = e.target.value
+    let foundInTheState = inTheState.indexOf(justChecked) //index or -1
+
+    //indexOf? 見つからない場合は-1を返し、そうでない場合はインデックスを返す
+    if (foundInTheState === -1) {
+      inTheState.push(justChecked)
+    } else {
+      // 見つかった場合は、インデックスから1つのアイテムを引き出す
+      inTheState.splice(foundInTheState, 1)
+    }
+    setCategoryIds(inTheState)
+    //console.log(inTheState);
+    fetchProducts({category: inTheState})
   }
 
   return (
@@ -70,7 +115,8 @@ const Shop = () => {
           <div className="col-md-3 pt-2 text-center">
             <h4>Search&Filter</h4>
             <hr/>
-            <Menu mode="inline" defaultOpenKeys={["1","2"]}>
+            <Menu mode="inline" defaultOpenKeys={["1", "2"]}>
+              {/* price */}
               <SubMenu
                 key="1"
                 title={
@@ -82,12 +128,25 @@ const Shop = () => {
                 <div>
                   <Slider
                     className="ml-4 mr-4"
-                    tipFormatter={(v) => `${v}円`}
+                    tipFormatter={(v) => `${v.toLocaleString()}円`}
                     range
                     value={price}
                     onChange={handleSlider}
                     max="300000"
                   />
+                </div>
+              </SubMenu>
+              {/* categories */}
+              <SubMenu
+                key="2"
+                title={
+                  <span className="h6">
+                    <DownSquareOutlined /> Category
+                  </span>
+                }
+              >
+                <div>
+                  {showCategories()}
                 </div>
               </SubMenu>
             </Menu>
