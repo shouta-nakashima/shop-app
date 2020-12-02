c980031dd869095b6c9c80d762a72c558bbea27a
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2020-12-02 09:45:22 +0900

    save cart to database

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,12 +1,21 @@
 const express = require('express')
+const { auth } = require('../firebase')
 
 const router = express.Router()
 
+//middlewares
+const {authCheck} = require('../middlewares/auth')
+
+//controllers
+const {userCart} = require('../controllers/user')
+
+router.post('/cart',authCheck, userCart)
+
 //route
-router.get('/user', (req, res) => {
-  res.json({
-    data: "hey you hit user API endpoint"
-  })
-})
+// router.get('/user', (req, res) => {
+//   res.json({
+//     data: "hey you hit user API endpoint"
+//   })
+// })
 
 module.exports = router
