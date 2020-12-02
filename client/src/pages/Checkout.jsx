9bae7686205175ceef6244f6e7412c2cb02b4123
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2020-12-02 11:13:26 +0900

    show cart items in checkout page

--- client/src/pages/Checkout.jsx
+++ client/src/pages/Checkout.jsx
@@ -39,15 +39,17 @@ const Checkout = () => {
         coupon input button
       </div>
       <div className="col-md-6">
-        <h4>注文内容</h4>
-        <h1>{ total}</h1>
-        {JSON.stringify(products)}
+        <h4>ご注文内容の確認</h4>
         <hr />
-        <p>Products x</p>
+        <p>現在{ products.length}つの商品が入っています</p>
         <hr />
-        <p>List of product</p>
+        {products.map((p,i) => (
+          <div key={i}>
+            <p>{p.product.title} ({p.color}) x {p.count} = { (p.product.price * p.count).toLocaleString()}円</p>
+          </div>
+        ))}
         <hr />
-        <p>カートの合計</p>
+        <p>カートの合計：{ total.toLocaleString()}円</p>
 
         <div className="row">
           <div className="col-md-6">
