abf9d16e6c4d43126592eccc1081a70cc273966c
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2020-12-02 10:35:40 +0900

    get user cart from database

--- server/routes/user.js
+++ server/routes/user.js
@@ -7,9 +7,10 @@ const router = express.Router()
 const {authCheck} = require('../middlewares/auth')
 
 //controllers
-const {userCart} = require('../controllers/user')
+const {userCart, getUserCart} = require('../controllers/user')
 
-router.post('/cart',authCheck, userCart)
+router.post('/user/cart', authCheck, userCart) //save cart
+router.get('/user/cart',authCheck, getUserCart) //get cart
 
 //route
 // router.get('/user', (req, res) => {
