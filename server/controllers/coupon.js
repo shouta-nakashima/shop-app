47587743881597682eb0e9b8eb79d2c503a8ce2e
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2020-12-02 15:38:16 +0900

    coupon routes and controllers

---
+++ server/controllers/coupon.js
@@ -0,0 +1,29 @@
+const Coupon = require('../models/coupon')
+
+
+exports.create = async (req, res) => {
+  try {
+    const { name, expiry, discount } = req.body
+    res.json(await new Coupon({name, expiry, discount}).save())
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+exports.list = async (req, res) => {
+  try {
+    res.json(await Coupon.findById({}).sort({createdAt: -1}).exec())
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+exports.remove = async (req, res) => {
+  try {
+    res.json(await Coupon.findByIdAndDelete(req.params.couponId).exec())
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+
