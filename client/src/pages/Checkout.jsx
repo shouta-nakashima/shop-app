686d1353664cb4d2905ea87aada3426b1e98879b
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2020-12-03 10:38:37 +0900

    apply coupon checkout

--- client/src/pages/Checkout.jsx
+++ client/src/pages/Checkout.jsx
@@ -11,13 +11,14 @@ const Checkout = () => {
   const [total, setTotal] = useState(0)
   const [address, setAddress] = useState("")
   const [addressSaved, setAddressSaved] = useState(false)
+  const [coupon, setCoupon] = useState('')
   const dispatch = useDispatch()
   const {user} = useSelector((state) => ({...state}))
 
   useEffect(() => {
     getUserCart(user.token)
       .then((res) => {
-        console.log('user cart res', JSON.stringify(res.data,null,4));
+        //console.log('user cart res', JSON.stringify(res.data,null,4));
         setProducts(res.data.products)
         setTotal(res.data.cartTotal)
       })
@@ -33,6 +34,43 @@ const Checkout = () => {
       }
     })
   }
+
+  const applyDiscountCoupon = () => {
+    console.log('send coupon backend', coupon);
+  }
+
+  const showAddress = () => (
+    <>
+      <ReactQuill theme="snow" value={address} onChange={setAddress}/>
+        <button
+          className="btn btn-primary mt-2 text-center"
+          onClick={saveAddressToDb}
+        >
+          SAVE
+        </button>
+    </>
+  )
+
+  const showProductSummary = () => 
+    products.map((p, i) => (
+    <div key={i}>
+      <p>
+        {p.product.title} ({p.color}) x {p.count} = {(p.product.price * p.count).toLocaleString()}円
+      </p>
+    </div>
+  ))
+
+  const showApplyCoupon = () => (
+    <>
+      <input 
+        value={coupon}
+        className="form-control"
+        type="text"
+        onChange={(e) => setCoupon(e.target.value)}
+      />
+      <button onClick={applyDiscountCoupon} className="btn btn-primary mt-2">Apply</button>
+    </>
+  )
 
   const emptyCart = () => {
     // localStorage からcartを削除
@@ -59,28 +97,18 @@ const Checkout = () => {
         <h4>配送先住所</h4>
         <br />
         <br />
-        <ReactQuill theme="snow" value={address} onChange={setAddress}/>
-        <button
-          className="btn btn-primary mt-2 text-center"
-          onClick={saveAddressToDb}
-        >SAVE</button>
+        {showAddress()}
         <hr />
         <h4>クーポンを使う</h4>
         <br />
-        coupon input button
+        {showApplyCoupon()}
       </div>
       <div className="col-md-6">
         <h4>ご注文内容の確認</h4>
         <hr />
         <p>現在{ products.length}つの商品が入っています</p>
         <hr />
-        {products.map((p,i) => (
-          <div key={i}>
-            <p>
-              {p.product.title} ({p.color}) x {p.count} = {(p.product.price * p.count).toLocaleString()}円
-            </p>
-          </div>
-        ))}
+        {showProductSummary()}
         <hr />
         <p>カートの合計：{ total.toLocaleString()}円</p>
 
