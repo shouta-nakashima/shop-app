2a7bf2daf0898e77e3031e3bab9227c1e1d3b2fb
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2020-12-04 10:55:58 +0900

    payment stripe successful cart total

--- server/controllers/stripe.js
+++ server/controllers/stripe.js
@@ -6,8 +6,15 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET)
 
 
 exports.createPaymentIntent = async (req, res) => {
+  //1 find user
+  const user = await User.findOne({email: req.user.email}).exec()
+  //2 get user cart total
+  const {cartTotal} = await Cart.findOne({orderdBy: user._id}).exec()
+
+  console.log('CART TOTAL CHARGE ===>', cartTotal);
+  //注文金額と通貨で支払いインテントを作成
   const paymentIntent = await stripe.paymentIntents.create({
-    amount: 100,
+    amount: cartTotal,
     currency: "usd"
   })
   res.send({
