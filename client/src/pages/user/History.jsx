35f109777bda30abe473012fd9c3c1f7330184be
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2020-12-07 08:59:28 +0900

    get user Orders frontend

--- client/src/pages/user/History.jsx
+++ client/src/pages/user/History.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import UserNav from '../../components/nav/UserNav'
+import { getUserOrders } from '../../functions/user'
+import { useDispatch, useSelector } from 'react-redux'
+import { CheckOutlined, CloseCircleOutlined } from '@ant-design/icons'
+import {toast} from 'react-toastify'
 
 const History = () => {
+  const [orders, setOrders] = useState([])
+  const { user } = useSelector((state) => ({ ...state }))
+  const dispatch = useDispatch()
+
+  const loadUserOrders = () => {
+    getUserOrders(user.token).then((res) => {
+      console.log(JSON.stringify(res.data, null, 4))
+      setOrders(res.data)
+    })
+    
+  }
+
+  useEffect(() => {
+    loadUserOrders()
+  }, [])
+  
   return (
     <div className ="container-fluid">
       <div className="row">
