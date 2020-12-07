144a650fa20c3466225248f07d9f7a1d2e8c3817
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2020-12-07 16:36:04 +0900

    get all orders admin dashboard frontend

--- client/src/pages/admin/AdminDashboard.jsx
+++ client/src/pages/admin/AdminDashboard.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 import AdminNav from '../../components/nav/AdminNav'
+import { getOrders, changeStatus } from '../../functions/admin'
+import { useDispatch, useSelector } from 'react-redux'
+import {toast} from 'react-toastify'
 
 
 const AdminDashboard = () => {
+  const [orders, setOrders] = useState([])
+  const { user } = useSelector((state) => ({ ...state }))
+
+  const loadOrders = () => getOrders(user.token)
+    .then((res) => {
+      console.log(JSON.stringify(res.data, null, 4));
+      setOrders(res.data)
+    })
   
+  useEffect(() => {
+    loadOrders()
+  },[])
+
+  const handleStatusChange = (orderId, orderStatus) => {
+    changeStatus(orderId, orderStatus, user.token)
+      .then((res) => {
+        console.log(res.data);
+        toast.success('取引情報を更新しました。')
+        loadOrders()
+      })
+  }
+
   return (
     <div className ="container-fluid">
       <div className="row">
@@ -12,6 +36,7 @@ const AdminDashboard = () => {
         </div>
         <div className="col">
           <h4 className="text-center pt-3 pb-3">Admin Dashboard</h4>
+          {JSON.stringify(orders)}
         </div>
       </div>
     </div>
