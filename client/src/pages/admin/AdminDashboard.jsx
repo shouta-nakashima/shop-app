ea408652f58dbdeb7f249a7ee9407458c8784a99
shouta-nakashima <fantajista5.6nakaji.s@gmail.com>
2020-12-08 09:31:53 +0900

    change order status complated

--- client/src/pages/admin/AdminDashboard.jsx
+++ client/src/pages/admin/AdminDashboard.jsx
@@ -2,7 +2,8 @@ import React,{useState, useEffect} from 'react'
 import AdminNav from '../../components/nav/AdminNav'
 import { getOrders, changeStatus } from '../../functions/admin'
 import { useDispatch, useSelector } from 'react-redux'
-import {toast} from 'react-toastify'
+import { toast } from 'react-toastify'
+import Orders from '../../components/order/Orders'
 
 
 const AdminDashboard = () => {
@@ -11,7 +12,7 @@ const AdminDashboard = () => {
 
   const loadOrders = () => getOrders(user.token)
     .then((res) => {
-      console.log(JSON.stringify(res.data, null, 4));
+      //console.log(JSON.stringify(res.data, null, 4));
       setOrders(res.data)
     })
   
@@ -22,7 +23,7 @@ const AdminDashboard = () => {
   const handleStatusChange = (orderId, orderStatus) => {
     changeStatus(orderId, orderStatus, user.token)
       .then((res) => {
-        console.log(res.data);
+        //console.log(res.data);
         toast.success('取引情報を更新しました。')
         loadOrders()
       })
@@ -34,9 +35,10 @@ const AdminDashboard = () => {
         <div className="col-md-2">
           <AdminNav/>
         </div>
-        <div className="col">
+        <div className="col-md-10">
           <h4 className="text-center pt-3 pb-3">Admin Dashboard</h4>
-          {JSON.stringify(orders)}
+          {/* {JSON.stringify(orders)} */}
+          <Orders orders={orders} handleStatusChange={handleStatusChange} />
         </div>
       </div>
     </div>
